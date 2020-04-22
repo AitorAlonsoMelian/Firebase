0858acfeb821f04eb3d29036732e8b929126857b
EduardoSY <61274696+EduardoSY@users.noreply.github.com>
2020-04-23 00:30:56 +0100

    Añadiendo funcion y limpiando

--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,31 @@
 //Fichero JS autentificación
-
+//Grupo 04 UyA - Aitor Alonso, Jesús Chamizo, Eduardo Da Silva
 const signupForm = document.querySelector('#formulario');
-signupForm.addEventListener('submit'(e) => {
-  e.preventDefault();
-    
-    const name = signupForm['first_name'].value;
-    const email = signupForm['signup_email'].value;
-    const password = signupForm['signup_password'].value;
-    const password2 = signupForm['signup_sec_password'].value;
-    const terms = signupForm['checkterms'].value;
+signupForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	//Obtenermos los valores del formulario
+	const name = signupForm['first_name'].value;
+	const email = signupForm['signup_email'].value;
+	const password = signupForm['signup_password'].value;
+	const password2 = signupForm['signup_sec_password'].value;
+	const terms = signupForm['checkterms'].value;
 
-    if((password==password2)&&(terms)){
-		// Registrar a los usuarios con los datos obtenidos
-		  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-			console.log(cred.user);
-			// Cerrar ventana y limpiardata
-			signupForm.reset();
-		  }).catch(function(error) {
-		  var errorMessage = error.message;
-		  window.alert("Algo ha pasado: " + errorMessage);
-		});
-	} else {
-		if(password!=password2){
-			window.alert("Las contraseñas no coinciden");
-		}else{
-			window.alert("Acepte los terminos de nuestra web.");
-		}
-	}
-
+    	if((password==password2)&&(terms)){
+          	var referencia = database.ref(name);
+      		//Escribir un documento
+		referencia.set({
+            		usuario: name,
+            		correo: email,
+           		contraseña: password
+        	});
+      		window.alert("Registrado correctamente");
+      		signupForm.reset();
+   	} else {
+        if(password!=password2){
+            window.alert("Las contraseñas no coinciden");
+        }else{
+            window.alert("Acepte los terminos de nuestra web.");
+        }
+    }
 });
